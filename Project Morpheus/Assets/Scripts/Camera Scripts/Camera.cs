50723428c735019dd4dc2ee68867ab2942a9ecf8
frederick <Frederick.van.der.Meulen@hva.nl>
2016-06-27 15:12:27 +0200

    Added basic movement and jumping values and camera

---
+++ Project Morpheus/Assets/Scripts/Camera Scripts/Camera.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+    public Transform target;
+    public float smoothing;
+
+    private Vector3 offset;
+    private Vector3 targetCameraPos;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - target.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        targetCameraPos = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCameraPos, smoothing * Time.deltaTime);
+	}
+}
---
+++ Project Morpheus/Assets/Scripts/Player Scripts/Movement.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    private Rigidbody rigidBody;
+    private float horizontalMovementSpeed;
+    private float verticalMovementSpeed;
+    public float jumpHeight;
+    private Vector3 movement;
+    private float distToGround;
+
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        movement = new Vector3(horizontalMovementSpeed, verticalMovementSpeed, jumpHeight);
+        distToGround = GetComponent<Collider>().bounds.extents.y;
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        //Handles the movement
+        horizontalMovementSpeed = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        verticalMovementSpeed = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        rigidBody.AddRelativeForce(horizontalMovementSpeed * -speed, 0, verticalMovementSpeed * -speed, ForceMode.Impulse);
+
+        //Handles the jumping
+        if(Input.GetKeyDown("space") && IsGrounded())
+        {
+            Debug.Log("Jump detected");
+            rigidBody.AddForce(transform.up * jumpHeight);
+        }
+	}
+
+    bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, -Vector3.up, distToGround);
+    }
+}
